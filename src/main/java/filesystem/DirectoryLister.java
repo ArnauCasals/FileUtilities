747d611575456d28bee7689b3efbdc3a23f63c45
Arnau <arnau@MacBook-de-Arnau.local>
2026-07-13 13:10:12 +0200

    Add recursive directory listing with file details

--- src/main/java/filesystem/DirectoryLister.java
+++ src/main/java/filesystem/DirectoryLister.java
@@ -1,6 +1,7 @@
 package filesystem;
 
 import java.io.File;
+import java.util.Date;
 import java.util.Arrays;
 
 public class DirectoryLister {
@@ -14,12 +15,28 @@ public class DirectoryLister {
 
         File directory = new File(args[0]);
 
+        listDirectory(directory, "");
+    }
+
+    private static void listDirectory(File directory, String indentation) {
+
         File[] files = directory.listFiles();
 
         Arrays.sort(files);
 
         for (File file : files) {
-            System.out.println(file.getName());
+
+            String type = file.isDirectory() ? "D" : "F";
+
+            Date lastModified = new Date(file.lastModified());
+
+            System.out.println(
+                    indentation + type + " " + file.getName() + " " + lastModified
+            );
+
+            if (file.isDirectory()) {
+                listDirectory(file, indentation + "    ");
+            }
         }
     }
 }
